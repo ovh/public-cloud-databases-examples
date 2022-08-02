fcf7518626da8ef71ad1883128746946b10c9cb7
Ludwig Patte <ludwig.patte@ovhcloud.com>
2022-08-02 15:27:34 +0000

    postgresql golang example

---
+++ databases/postgresql/go/hello-world/example.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	args := os.Args
+	if len(args) != 2 {
+		log.Fatal(fmt.Errorf("URI is missing in the environment"))
+	}
+
+	db, err := sql.Open("postgres", args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var version string
+	err = db.QueryRow("select version()").Scan(&version)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(version)
+}
---
+++ databases/postgresql/go/hello-world/go.mod
@@ -0,0 +1,5 @@
+module github.com/ovh/public-cloud-databases-training-examples/databases/postgresql/go/hello-world
+
+go 1.18
+
+require github.com/lib/pq v1.10.6
